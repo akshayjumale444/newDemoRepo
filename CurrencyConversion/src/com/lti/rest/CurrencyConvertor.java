31bea0760cb72b3a786cb71e9eb826f29920b939
Me Me <me@gmail.com>
2019-07-25 09:51:15 -0700

    Commiting "Currency Conversion project"

---
+++ CurrencyConversion/src/com/lti/rest/CurrencyConvertor.java
@@ -0,0 +1,21 @@
+package com.lti.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/currency")
+public class CurrencyConvertor
+{
+	@GET
+	@Path("/{rs}")
+	@Produces(MediaType.TEXT_HTML)
+	public String rsToDollar(@PathParam("rs") double rs)
+	{
+		double d=rs/75;
+		return "<h1>"+rs+"/Rs. Dollar="+d+"</h1>";
+	}
+	
+}
